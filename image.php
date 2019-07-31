290378fd320cde153479979951b02f50bb62efd0
WebSEC.nl <rebitclown105@hotmail.com>
2019-07-31 23:13:08 +0200

    Create image.php

---
+++ image.php
@@ -0,0 +1,8 @@
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+$agent =  $_SERVER['HTTP_USER_AGENT'];
+$file    = 'burp.log';
+$handle = fopen($file, 'a');
+fputs($handle, "BURP [IP: $ip, UA: $agent\n\n");
+fclose($handle);
+?>
