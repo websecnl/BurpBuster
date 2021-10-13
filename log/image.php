3a1416c6e0cbfc01760722545475f4f251b847fa
WebSEC.nl <rebitclown105@hotmail.com>
2021-10-14 01:36:48 +0200

    Delete image.php

--- log/image.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ip = $_SERVER['REMOTE_ADDR'];
-$agent =  $_SERVER['HTTP_USER_AGENT'];
-$file    = 'burp.log';
-$handle = fopen($file, 'a');
-fputs($handle, "BURP [IP: $ip, UA: $agent\n\n");
-fclose($handle);
-?>
- 
